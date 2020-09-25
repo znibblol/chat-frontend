c9485dba271ff7b792d939fee050febca4449eeb
Emil Malm <emilamalm@gmail.com>
2020-09-25 08:52:30 +0200

    :tada: intial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap');
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <div class="wrapper">
+      <div class="left-panel">
+        <h3>ChatUP</h3>
+      </div>
+      <div class="chat-main"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home"
+};
+</script>
+
+<style>
+ body {
+   margin: 0;
+   padding: 0;
+   box-sizing: 0;
+   font-family: 'Montserrat', sans-serif;
+ }
+  .wrapper {
+    width: 100%;
+    display: grid;
+    grid-template-columns: 2fr 8fr;
+    height: 100vh;
+  }
+  .left-panel {
+    width: 100%;
+    height: 100%;
+    padding: 15px;
+  }
+  .chat-main {
+    width: 100%;
+    height: 100%;
+  }
+</style>
